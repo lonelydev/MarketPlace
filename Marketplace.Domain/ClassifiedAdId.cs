3bda84bb06372e3adaf4523c82bd39323f5dc980
Eakan Gopalakrishnan <me_theonlyone@hotmail.com>
2021-05-14 09:10:22 +0100

    feat(valueobjects): Money, Ids
    some tests added

---
+++ Marketplace.Domain/ClassifiedAdId.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Marketplace.Domain
+{
+    /// <summary>
+    /// Enforce constraints at the Value Object level
+    /// This helps make code more explicit
+    /// </summary>
+    public class ClassifiedAdId
+    {
+        private readonly Guid _value;
+        public ClassifiedAdId(Guid value)
+        {
+            if (value == default)
+            {
+                throw new ArgumentNullException(nameof(value), "Classified Ad id cannot be empty");
+            }
+            _value = value;
+        }
+    }
+}
---
+++ Marketplace.Tests/MoneyTest.cs
@@ -0,0 +1,16 @@
+﻿using Marketplace.Domain;
+using Xunit;
+
+namespace Marketplace.Tests
+{
+    public class MoneyTest
+    {
+        [Fact]
+        public void Money_ObjectsWithSameAmount_ShouldBeEqual()
+        {
+            var firstAmount = new Money(5);
+            var secondAmount = new Money(5);
+            Assert.Equal(firstAmount, secondAmount);
+        }
+    }
+}
