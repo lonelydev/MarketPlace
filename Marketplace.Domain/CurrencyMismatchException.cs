5e4e9773e3e596e1139a4291ecccccfcdfef4212
Eakan Gopalakrishnan <me_theonlyone@hotmail.com>
2021-05-15 15:12:32 +0100

    feat(supportcurr): Currency and Money
    Add currency to money.
    Add service to validate currency.
    use it later.

---
+++ Marketplace.Domain/CurrencyMismatchException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Marketplace.Domain
+{
+    [Serializable]
+    internal class CurrencyMismatchException : Exception
+    {
+        public CurrencyMismatchException()
+        {
+        }
+
+        public CurrencyMismatchException(string message) : base(message)
+        {
+        }
+
+        public CurrencyMismatchException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected CurrencyMismatchException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
--- Marketplace.Domain/Money.cs
+++ Marketplace.Domain/Money.cs
@@ -11,6 +11,7 @@ namespace Marketplace.Domain
     /// </summary>
     public class Money : Value<Money>
     {
+        private const string DefaultCurrency = "EUR";
         /// <summary>
         /// Factory functions to create money from different formats of inputs
         /// decimal and string. In combination with a non-public constructor, this streamlines
@@ -18,11 +19,12 @@ namespace Marketplace.Domain
         /// </summary>
         /// <param name="amount"></param>
         /// <returns></returns>
-        public static Money FromDecimal(decimal amount) => new Money(amount);
-        public static Money FromString(string amount) => new Money(decimal.Parse(amount));
+        public static Money FromDecimal(decimal amount, string currency = DefaultCurrency) => new Money(amount, currency);
+        public static Money FromString(string amount, string currency = DefaultCurrency) => new Money(decimal.Parse(amount), currency);
         
         public decimal Amount { get; }
-        protected Money(decimal amount)
+        public string CurrencyCode { get; }
+        protected Money(decimal amount, string currencyCode = DefaultCurrency)
         {
             if (decimal.Round(amount, 2) != amount)
             {
@@ -30,11 +32,27 @@ namespace Marketplace.Domain
             }
 
             Amount = amount;
+            CurrencyCode = currencyCode;
         }
 
-        public Money Add(Money summand) => new Money(Amount + summand.Amount);
+        public Money Add(Money summand)
+        {
+            if (CurrencyCode != summand.CurrencyCode)
+            {
+                throw new CurrencyMismatchException("Cannot sum amounts with different currencies");
+            }
+            return new Money(Amount + summand.Amount);
+        }
 
-        public Money Subtract(Money subtrahend) => new Money(Amount - subtrahend.Amount);
+        public Money Subtract(Money subtrahend)
+        {
+            if (CurrencyCode != subtrahend.CurrencyCode)
+            {
+                throw new CurrencyMismatchException("Cannot subtract amounts with different currencies");
+            }
+
+            return new Money(Amount - subtrahend.Amount);
+        }
 
         public static Money operator +(Money summand1, Money summand2) => summand1.Add(summand2);
 
