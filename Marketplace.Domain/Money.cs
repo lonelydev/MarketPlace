a166d62560753a80b884967c5cd0af0d700f45b1
Eakan Gopalakrishnan <me_theonlyone@hotmail.com>
2021-05-15 10:53:28 +0100

    feat(valueobject): Framework and ValueObjects
    Introducing first Generic Value object implementation in Framework.
    Money is now a child of Value<Money>
    Test pass.

--- Marketplace.Domain/Money.cs
+++ Marketplace.Domain/Money.cs
@@ -1,51 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Marketplace.Framework;
 
 namespace Marketplace.Domain
 {
     /// <summary>
     /// Implement IEquatable<T> ensures that you can compare two instances of Money
     /// </summary>
-    public class Money : IEquatable<Money>
+    public class Money : Value<Money>
     {
         public decimal Amount { get; set; }
-        public Money(decimal amount)
-        {
-            Amount = amount;
-        }
-
-        public bool Equals([AllowNull] Money other)
-        {
-            if (ReferenceEquals(null, other)) return false;
-            if (ReferenceEquals(this, other)) return true;
-            return Amount.Equals(other.Amount);
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return Equals((Money)obj);
-        }
-
-        public override int GetHashCode()
-        {
-            return Amount.GetHashCode();
-        }
-
-        public static bool operator ==(Money left, Money right)
-        {
-            return Equals(left, right);
-        }
-
-        public static bool operator !=(Money left, Money right)
-        {
-            return !Equals(left, right);
-        }
+        public Money(decimal amount) => Amount = amount;
     }
 }
